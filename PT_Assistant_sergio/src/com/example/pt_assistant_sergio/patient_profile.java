8a38e16ad87fef0d73d87197c9bf305f217d17ad
Sergio DiVentura <sergiodiv@gmail.com>
2014-06-07 14:03:32 -0400

    added a junit test

--- PT_Assistant_sergio/src/com/example/pt_assistant_sergio/patient_profile.java
+++ PT_Assistant_sergio/src/com/example/pt_assistant_sergio/patient_profile.java
@@ -5,14 +5,20 @@ public class patient_profile {
 	
 	patient current_patient;
 	public void create_patient_profile(String name, int ID, int injury_ID)
-	{
-		//current_patient.injury = injury_ID;
-		//current_patient.name = name;
-		//current_patient.patientID  = injury_ID;
+	{   
+		
+		current_patient = new patient();
+		current_patient.injury = injury_ID;
+		current_patient.name = name;
+		current_patient.patientID  = ID;
 		
 		
 	}
-
+	public patient get_patient_profile()
+	{
+		return current_patient;
+		
+	}
 	
 
 }
--- PT_Assistant_sergio/src/com/example/pt_assistant_sergio/patient_profileTest.java
+++ PT_Assistant_sergio/src/com/example/pt_assistant_sergio/patient_profileTest.java
@@ -5,8 +5,22 @@ import junit.framework.TestCase;
 public class patient_profileTest extends TestCase {
 
 	public void testCreate_patient_profile() {
-		//fail("Not yet implemented");
-		System.out.print("Hello ");
+		String name = "Sergio";
+		int patientId = 25;
+		int injury_ID = 1234;
+		
+		patient_profile newPatientTest = new patient_profile();
+		patient result_patient = new patient();
+		
+		newPatientTest.create_patient_profile(name, patientId, injury_ID);
+		
+		result_patient = newPatientTest.get_patient_profile( );
+		
+		assertEquals(name, result_patient.name);
+		assertEquals(patientId, result_patient.patientID);
+		assertEquals(injury_ID, result_patient.injury);
+		
+		
 		
 		
 		
