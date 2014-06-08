1be0a6c9a31a54435b0a92e2ae225177a86f15a0
Sergio DiVentura <sergiodiv@gmail.com>
2014-06-08 13:01:33 -0400

    renamed the top level folder

---
+++ PT_Assistant/src/com/example/pt_assistant_sergio/MainActivity.java
@@ -0,0 +1,103 @@
+
+package com.example.pt_assistant_sergio;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+public class MainActivity extends  ActionBarActivity{
+	public final static String EXTRA_MESSAGE = "com.example.pt_assistant_sergio.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+        
+        setContentView(R.layout.activity_display_message);
+
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        // If your minSdkVersion is 11 or higher, instead use:
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        
+        
+        
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+    	MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_main_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+       
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	int itemId = item.getItemId();
+			if (itemId == R.id.action_search) {
+				openSearch();
+				return true;
+			} else if (itemId == R.id.action_settings) {
+				openSettings();
+				return true;
+			} else {
+				return super.onOptionsItemSelected(item);
+			}
+        
+    }
+
+    private void openSettings() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	private void openSearch() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	EditText editText = (EditText) findViewById(R.id.edit_message );
+    	String message = editText.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	startActivity(intent);
+    }
+   
+  
+    
+
+}
---
+++ PT_Assistant/src/com/example/pt_assistant_sergio/Patient_ProfileTest.java
@@ -0,0 +1,27 @@
+package com.example.pt_assistant_sergio;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class Patient_ProfileTest extends TestCase {
+
+	@Test
+	public void testCreate_patient_profile() {
+		
+		//create Patient object
+		Patient p = new Patient();
+		
+		//set the Patient's ID, Name, and Injury
+		p.setPatientID(1);
+		p.setName("John Doe");
+		p.setInjury(5);
+		
+		/*Asserts that two integers are equal. The "create_patient" method 
+		 * will return an integer (either 1 for the row count of a successful
+		 * SQL statement or a 0 for SQL statements that return nothing
+		*/
+		assertEquals(1, p.create_patient());
+	}
+
+}
