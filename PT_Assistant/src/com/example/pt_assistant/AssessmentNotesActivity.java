21139a9f475c26c6a94d632a2d103bc90a4465c1
rahalloran <rahalloran119@gmail.com>
2014-07-20 22:10:11 -0400

    spinner updates

--- PT_Assistant/src/com/example/pt_assistant/AssessmentNotesActivity.java
+++ PT_Assistant/src/com/example/pt_assistant/AssessmentNotesActivity.java
@@ -31,32 +31,27 @@ public class AssessmentNotesActivity extends ActionBarActivity implements OnItem
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_assessment_notes);
+		setContentView(R.layout.fragment_assessment_notes);
 
 		
-		if (savedInstanceState == null) {
+		/*if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		}*/
         // Spinner element
-        Spinner spinner = (Spinner) findViewById(R.id.spinnerInjury);
+        Spinner spin = (Spinner) findViewById(R.id.spinnerInjury);
  
         // Spinner click listener
-        spinner.setOnItemSelectedListener(this);
- 
-        // Spinner Drop down elements
-        List<String> categories = new ArrayList<String>();
-        categories.add("LOWER BACK INJURY LUMBAR");
-        categories.add("ROTATOR CUFF TENDINITIS");
+        spin.setOnItemSelectedListener(this);
  
         // Creating adapter for spinner
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.array.injury_array, android.R.layout.simple_spinner_item);
  
         // Drop down layout style - list view with radio button
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
  
         // attaching data adapter to spinner
-        spinner.setAdapter(dataAdapter);
+        spin.setAdapter(adapter);
 		
 	}
 
@@ -91,9 +86,9 @@ public class AssessmentNotesActivity extends ActionBarActivity implements OnItem
 		eText = (EditText) findViewById(R.id.editAssessment);
 		pn.setPatient_diagnosis(eText.getText().toString());
 		
-		Spinner spin;
-		spin = (Spinner) findViewById(R.id.spinnerInjury);
-		pn.setInjury(spin.getItemAtPosition(0).toString());//probably not right need to resolve
+		//Spinner spin;
+		//spin = (Spinner) findViewById(R.id.spinnerInjury);
+		//pn.setInjury(adapter.getPosition().toString());//probably not right need to resolve
 		//need the actual position value from below in onItemSelected
 
 		//Serialize, start next activity and send intent
@@ -126,8 +121,6 @@ public class AssessmentNotesActivity extends ActionBarActivity implements OnItem
 	public void onItemSelected(AdapterView<?> pn, View view, int position,
 			long id) {
         String item = pn.getItemAtPosition(position).toString();
-        //it seems like we want to use this string value to set to the database but
-        //it doesn't let us set it from here using the adapterview
  
         // Showing selected spinner item
         //Toast.makeText(pn.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
